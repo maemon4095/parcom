f20a8d834e8887d4f8a84494f1271818ac96c042
maemon4095 <maemondecocted@gmail.com>
2024-01-09 06:04:09 +0900

    either never

--- parcom/src/standard.rs
+++ parcom/src/standard.rs
@@ -3,7 +3,7 @@ pub mod parse;
 pub mod parser;
 
 use crate::ShouldNever;
-use std::ops::Bound;
+use std::{fmt::Debug, ops::Bound};
 
 pub use parser::ParserExtension;
 
@@ -19,6 +19,56 @@ fn just_on_boundary(item: usize, bound: Bound<&usize>) -> bool {
 pub enum Either<T0, T1> {
     First(T0),
     Last(T1),
+}
+
+impl<T0, T1> Either<T0, T1> {
+    pub fn unwrap_first(self) -> T0
+    where
+        T1: Debug,
+    {
+        match self {
+            Either::First(e) => e,
+            Either::Last(e) => {
+                panic!(
+                    "`Either::unwrap_first` was called on an `Either::Last({:?})`.",
+                    e
+                )
+            }
+        }
+    }
+
+    pub fn unwrap_last(self) -> T1
+    where
+        T0: Debug,
+    {
+        match self {
+            Either::First(e) => panic!(
+                "`Either::unwrap_last` was called on an `Either::First({:?})`.",
+                e
+            ),
+            Either::Last(e) => e,
+        }
+    }
+
+    pub fn always_first(self) -> T0
+    where
+        T1: ShouldNever,
+    {
+        match self {
+            Either::First(e) => e,
+            Either::Last(e) => e.never(),
+        }
+    }
+
+    pub fn always_last(self) -> T1
+    where
+        T0: ShouldNever,
+    {
+        match self {
+            Either::First(e) => e.never(),
+            Either::Last(e) => e,
+        }
+    }
 }
 
 impl<T0: ShouldNever, T1: ShouldNever> ShouldNever for Either<T0, T1> {}
